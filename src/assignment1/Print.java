0d9b0713e676fcb6269b092318418418a02cfef9
shwetajadhavss <jadhavshweta5056@gmail.com>
2022-04-01 15:39:00 +0530

    Initial commit

---
+++ src/assignment1/Print.java
@@ -0,0 +1,16 @@
+/* Write the program to print the Velocity Corporate training center
+ message on console*/
+ 
+package assignment1;
+
+public class Print{
+	
+	public static void main(String []args){
+		
+		System.out.println("Velocity Corporate training center");
+		
+		
+	
+	}
+
+}
